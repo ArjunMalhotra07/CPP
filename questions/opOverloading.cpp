729ca22d945dce565e6343c235c8d3aa4691f048
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-12-11 18:31:18 +0530

    Operator Overloading

--- questions/opOverloading.cpp
+++ questions/opOverloading.cpp
@@ -18,13 +18,41 @@ public:
         cout << "Copied is : " << s << endl; 
     }
 };
+void callbacks1(){
+    AddString a1, a2, a4; 
+	a1.setName("Arjun "); 
+    a2.setName("M");
+    a4.setName(" is my name");
+    AddString a3 = a1 < a2 < a4;
+    a3.print();
+}
+class AddString1 {
+private:
+	string s;
+public:
+    void setName(string str){
+        s = str;
+    }
+
+    AddString1 operator < (AddString1 &obj){
+        AddString1 res;
+        res.s = obj.s;
+        return res;
+    }
+    void print(){
+        cout << "String is : " << s << endl; 
+    }
+};
+void callbacks2(){
+    AddString1 a1, a2; 
+	a1.setName("Arjun ");
+    a2.setName("M "); 
+    AddString1 a3 = a1 < a2 ;
+    a3.print();
+}
 
 int main()
 {   
-    AddString a1, a2; 
-	a1.setName("Arjun "); 
-    a2.setName("M");
-    AddString a3 = a1 < a2;
-    a3.print();
+    callbacks2();
 	
 }
